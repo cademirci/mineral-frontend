52bb65220b31f90d161d9a5ebca0799ee069ad97
Çağlayan <demircicaglayan13@gmail.com>
2023-12-05 04:27:16 +0300

    add a welcoming screen

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, Image, TouchableOpacity } from 'react-native';
 import { useEffect, useRef, useState } from 'react';
 import { Camera } from 'expo-camera';
 import { shareAsync } from 'expo-sharing';
 import * as MediaLibrary from 'expo-media-library';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+       Welcome to Mineral App. 
+      </Text>
+      <TouchableOpacity
+        style={styles.defaultButton}
+        title="take a picture"
+        onPress={() => navigation.navigate('PhotoScreen')}
+      >
+        <Text style={styles.defaultText}>
+          Take a Picture
+        </Text>
+      </ TouchableOpacity>
+    </View>
+  )
+}
+
+function PhotoScreen({ navigation }) {
   let cameraRef = useRef();
   const [hasCameraPermission, setHasCameraPermission] = useState();
   const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
@@ -72,6 +93,19 @@ export default function App() {
   );
 }
 
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator initialRouteName="mineral">
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="PhotoScreen" component={PhotoScreen} />
+      </Stack.Navigator>   
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -82,8 +116,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignSelf: 'flex-end'
   },
+  defaultButton: {
+    backgroundColor: '#2b6d74',
+    padding: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    margin: 20,
+    borderRadius: 20,
+  },
   preview: {
     alignSelf: 'stretch',
     flex: 1
+  },
+  defaultText: {
+    color: '#fff',
+  },
+  paragraph: {
+    color: '#1a1a1a',
+    textAlign: 'center',
+    margin: 100
   }
 });
