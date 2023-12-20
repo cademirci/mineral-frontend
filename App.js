79e25ede8a9ecceae073121295df9f1064ad4e21
Çağlayan <demircicaglayan13@gmail.com>
2023-12-20 14:19:17 +0300

    development process

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, Button, Image, TouchableOpacity, TextInput } from 'react-native';
 import { useEffect, useRef, useState } from 'react';
 import { Camera } from 'expo-camera';
 import { shareAsync } from 'expo-sharing';
 import * as MediaLibrary from 'expo-media-library';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { RadioButton } from 'react-native-paper';
+import {
+  azuriteProperties,
+  baryteProperties,
+  berylProperties,
+  calciteProperties,
+  cerussiteProperties,
+  copperProperties,
+  hematiteProperties,
+  malachiteProperties,
+  pyriteProperties,
+  pyromorphiteProperties,
+  quartzProperties,
+  wulfeniteProperties
+} from './minprop'
 
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>
-       Welcome to Mineral App. 
+        Welcome to Mineral App. Try to take a picture of a mineral to identify.
       </Text>
       <TouchableOpacity
         style={styles.defaultButton}
@@ -22,7 +37,96 @@ function HomeScreen({ navigation }) {
           Take a Picture
         </Text>
       </ TouchableOpacity>
+      <TouchableOpacity
+        style={styles.alterButton}
+        title="login"
+        onPress={() => navigation.navigate('Login')}
+      >
+        <Text style={styles.defaultText}>
+          Login
+        </Text>
+      </ TouchableOpacity>
+      <TouchableOpacity
+        style={styles.alterButton}
+        title="sign up"
+        onPress={() => navigation.navigate('SignUp')}
+      >
+        <Text style={styles.defaultText}>
+          Sign Up
+        </Text>
+      </ TouchableOpacity>
     </View>
+  )
+}
+
+function SignUp({ navigation }) {
+  const [text, onChangeText] = useState('');
+  const [number, onChangeNumber] = useState('');
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="username"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="mail address"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="password"
+        // keyboardType="numeric"
+      />
+      <TouchableOpacity
+        style={styles.defaultButton}
+        title="register"
+        // galeriye gonderme 
+        onPress={() => navigation.navigate('')}
+      >
+        <Text style={styles.defaultText}>
+          Sign Up
+        </Text>
+      </ TouchableOpacity>
+    </SafeAreaView>
+  )
+}
+
+function Login({ navigation }) {
+  const [text, onChangeText] = useState('');
+  const [number, onChangeNumber] = useState(''); 
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="username"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="password"
+        // keyboardType="numeric"
+      />
+      <TouchableOpacity
+        style={styles.defaultButton}
+        title="register"
+        onPress={() => navigation.navigate('')}
+      >
+        <Text style={styles.defaultText}>
+          Sign Up
+        </Text>
+      </ TouchableOpacity>
+    </SafeAreaView>
   )
 }
 
@@ -76,9 +180,22 @@ function PhotoScreen({ navigation }) {
     return (
       <SafeAreaView style={styles.container}>
         <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} />
-        <Button title="Share" onPress={sharePic} />
-        {hasMediaLibraryPermission ? <Button title="Save" onPress={savePhoto} /> : undefined}
-        <Button title="Discard" onPress={() => setPhoto(undefined)} />
+        <TouchableOpacity 
+          title="Share" 
+          onPress={() => navigation.navigate('DetailedReport')}
+          style={styles.defaultButton} 
+        > 
+          <Text style={styles.defaultText}>
+            Analyze
+          </Text>
+        </ TouchableOpacity>
+        <View style={styles.flexView}>
+          <Button title="Share" onPress={sharePic} />
+          {hasMediaLibraryPermission ? 
+          <Button title="Save" onPress={savePhoto} /> : undefined}
+          <Button title="Discard"  onPress={() => setPhoto(undefined)} />
+        </View>
+        
       </SafeAreaView>
     );
   }
@@ -86,12 +203,163 @@ function PhotoScreen({ navigation }) {
   return (
     <Camera style={styles.container} ref={cameraRef}>
       <View style={styles.buttonContainer}>
-        <Button title="Take Pic" onPress={takePic} />
+        <TouchableOpacity 
+            title="takePicture" 
+            onPress={takePic} 
+            style={styles.takePicButton} 
+          > 
+          <Text style={styles.defaultText}>
+            Take picture
+          </Text>
+        </ TouchableOpacity>
       </View>
       <StatusBar style="auto" />
     </Camera>
   );
 }
+
+function SeeTheResults({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        The analysis is completed. 
+      </Text>
+      <TouchableOpacity
+        style={styles.defaultButton}
+        title="seeTheRsults"
+        onPress={() => navigation.navigate('DetailedReport')}
+      >
+        <Text style={styles.defaultText}>
+          See the results
+        </Text>
+      </ TouchableOpacity>
+
+    </View>
+  )
+}
+
+function DetailedReport({ navigation }) {
+  return (
+    <ScrollView style={styles.scrollContainer}>
+      <Text style={styles.mineraltitle}>
+       Azurit
+      </Text>
+      <Text style={styles.infoParagraph}>
+       detailed information about the mineral
+      </Text>
+      <TouchableOpacity
+        style={styles.defaultButton}
+        title="seeTheRsults"
+        onPress={() => navigation.navigate('PoemScreen')}
+      >
+        <Text style={styles.defaultText}>
+          Write a Poem or a Story
+        </Text>
+      </ TouchableOpacity>
+    </ScrollView>
+  )
+}
+
+function PoemScreen({ navigation }) {
+  const [poemLines, setPoemLines] = useState('option1');
+  const [genre, setGenre] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Text>Poem or Story?</Text>
+      <RadioButton.Group
+        onValueChange={(value) => setGenre(value)}
+        value={genre}
+      >
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <RadioButton value="option1" color="blue" />
+          <Text>Poem</Text>
+        </View>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <RadioButton value="option2" color="blue" />
+          <Text>Story</Text>
+        </View>
+      </RadioButton.Group>
+
+      <Text>Select number of line in the poem</Text>
+      <RadioButton.Group
+        onValueChange={(value) => setPoemLines(value)}
+        value={poemLines}
+      >
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <RadioButton value="option1" color="green" />
+          <Text>4</Text>
+        </View>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <RadioButton value="option2" color="green" />
+          <Text>5</Text>
+        </View>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <RadioButton value="option3" color="green" />
+          <Text>6</Text>
+        </View>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <RadioButton value="option3" color="green" />
+          <Text>7</Text>
+        </View>
+      </RadioButton.Group>
+      {/* <Text>Selected Value: {poemLines}</Text> */}
+
+      <TouchableOpacity
+        style={styles.defaultButton}
+        title="seeTheRsults"
+        onPress={() => navigation.navigate('DisplayPoem')}
+      >
+        <Text style={styles.defaultText}>
+          Write
+        </Text>
+      </ TouchableOpacity>
+      
+    </View>
+  );
+}
+
+function DisplayPoem({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+       Azurit
+      </Text>
+      <Text style={styles.paragraph}>
+       Azurit
+      </Text>
+
+    </View>
+  )
+}
+
+function Fovourites({ navigation }) {
+  return (
+    <View style={styles.container}>
+
+
+    </View>
+  )
+}
+
+function History({ navigation }) {
+  return (
+    <View style={styles.container}>
+
+
+    </View>
+  )
+}
+
+function Feedback({ navigation }) {
+  return (
+    <View style={styles.container}>
+
+
+    </View>
+  )
+}
+
 
 const Stack = createNativeStackNavigator();
 
@@ -100,29 +368,56 @@ export default function App() {
     <NavigationContainer style={styles.container}>
       <Stack.Navigator initialRouteName="mineral">
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SignUp" component={SignUp} />
         <Stack.Screen name="PhotoScreen" component={PhotoScreen} />
-      </Stack.Navigator>   
+        <Stack.Screen name="DetailedReport" component={DetailedReport} />
+        <Stack.Screen name="PoemScreen" component={PoemScreen} />
+        <Stack.Screen name="SeeTheResults" component={SeeTheResults} />
+        <Stack.Screen name="History" component={History} />
+        <Stack.Screen name="Feedback" component={Feedback} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flex: 1
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  buttonContainer: {
-    backgroundColor: '#fff',
-    alignSelf: 'flex-end'
+  buttonContainer: { 
+    marginTop: 400,
   },
   defaultButton: {
     backgroundColor: '#2b6d74',
+    maxWidth: '50%',
     padding: 10,
     paddingLeft: 20,
     paddingRight: 20,
     margin: 20,
     borderRadius: 20,
+  },
+  alterButton: {
+    backgroundColor: '#8b8b8b',
+    maxWidth: '50%',
+    padding: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    margin: 20,
+    borderRadius: 20,
+  },
+  takePicButton: {
+    backgroundColor: '#2b6d74',
+    padding: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderRadius: 20,
+    marginBottom: 0
   },
   preview: {
     alignSelf: 'stretch',
@@ -130,10 +425,30 @@ const styles = StyleSheet.create({
   },
   defaultText: {
     color: '#fff',
+    textAlign: 'center'
   },
   paragraph: {
     color: '#1a1a1a',
     textAlign: 'center',
     margin: 100
+  },
+  infoParagraph: {
+    color: '#1a1a1a',
+    margin: 25
+  },
+  mineraltitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    margin: 10
+  },
+  flexView: {
+    flexDirection: 'row'
+  },
+  input: {
+    height: 40,
+    minWidth: '75%',
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   }
 });
---
+++ minprop.js
@@ -0,0 +1,183 @@
+//use console.log(JSON.stringify(CONST_NAME, null, 2)); to output the details
+
+export const azuriteProperties = {
+  Azurite: {
+    Name: "Azurite", 
+    Formula: "Cu3(CO3)2(OH)2",
+    Contents: "Copper, Hydrogen, Carbon, Oxygen",
+    Colour: "Blue, Light Blue, Dark Blue ",
+    Lustre: "Vitreous",
+    Hardness: "3½ - 4",
+    SpecificGravity: "3.77",
+    CrystalSystem: "Monoclinic",
+    Origin: "From the ancient Persian lazhward, meaning 'blue', in allusion to the color. Name changed to azurite in 1824 by François Sulpice Beudant.",
+    Locality: ["Adana", "Balıkesir", "Burdur", "Çanakkale"]
+  }
+};
+
+export const baryteProperties = {
+  Baryte: {
+    Name: "Baryte", 
+    Formula: "BaSO4",
+    Contents: "Barium, Sulphur, Oxygen",
+    Colour: "Colourless, White, Yellow, Brown, Grey, Blue",
+    Lustre: "Vitreous, Pearly",
+    Hardness: 3,
+    SpecificGravity: "4.50",
+    CrystalSystem: "Orthorhombic",
+    Origin: "Named in 1800 by Dietrich Ludwig Gustav Karsten from the Greek βαρύς, heavy, due to its unusual heaviness for a non-metallic mineral.",
+    Locality: ["Elazığ", "Diyarbakır"]
+  }
+};
+
+export const berylProperties = {
+  Beryl: {
+    Name: "Beryl", 
+    Formula: "Be3Al2(Si6O18)",
+    Contents: "Beryllium, Aliminium, Silicon",
+    Colour: "Colorless, Green, Blue, Yellow, White, Pink",
+    Lustre: "Vitreous, Sub-Vitreous, Waxy, Greasy",
+    Hardness: "7½ - 8",
+    SpecificGravity: "2.63 - 2.92",
+    CrystalSystem: "Hexagonal",
+    Origin: "From the Greek 'beryllos' which referred to a number of blue-green stones in antiquity.",
+    Locality: ["Eskişehir"]
+  }
+};
+
+export const calciteProperties = {
+  Calcite: {
+    Name: "Calcite", 
+    Formula: "CaCO3",
+    Contents: "Calcium, Carbon, Oxygen",
+    Colour: "White, Yellow, Red, Orange, Blue, Green, Brown, Gray ",
+    Lustre: "Vitreous, Sub-Vitreous, Resinous, Waxy, Pearly",
+    Hardness: 3,
+    SpecificGravity: "2.7102",
+    CrystalSystem: "Trigonal",
+    Origin: "Named as a mineral by Gaius Plinius Secundus (Pliny the elder) in 79 from Calx, Latin for Lime.",
+    Locality: ["Bursa","Erzurum","Iğdır"]
+  }
+};
+
+export const cerussiteProperties = {
+  Cerussite: {
+    Name: "Cerussite", 
+    Formula: "PbCO3",
+    Contents: "Lead, Carbon, Oxygen",
+    Colour: "Colourless, White, Gray, Blue, Green",
+    Lustre: "Adamantine, Vitreous, Resinous, Pearly, Dull, Earthy",
+    Hardness: "3 - 3½",
+    SpecificGravity: "6.53 - 6.57",
+    CrystalSystem: "Orthorhombic",
+    Origin: "Named in 1845 by Wilhelm Karl von Haidinger from the Latin, cerussa, meaning 'white lead.'",
+    Locality: ["Nevşehir", "Niğde", "Kırşehir"]
+  }
+};
+
+export const copperProperties = {
+  Copper: {
+    Name: "Copper", 
+    Formula: "Cu",
+    Contents: "Copper",
+    Colour: "Brown-Red",
+    Lustre: "Metallic",
+    Hardness: "2½ - 3",
+    SpecificGravity: "8.94 - 8.95",
+    CrystalSystem: "Isometric",
+    Origin: "From Greek 'kyprios', of Cyprus, the location of ancient copper mines; Latin 'cuprum'.",
+    Locality: ["Afyon", "Aksaray", "Artvin", "Balıkesir", "Bursa", "Denizli", "Elazığ", "Trabzon"]
+  }
+};
+
+export const hematiteProperties = {
+  HematiteHide: {
+    Name: "Hematite", 
+    Formula: "Fe2O3",
+    Contents: "Iron, Oxygen",
+    Colour: "Steel-Grey, Black",
+    Lustre: "Metallic, Sub-Metallic, Dull, Earthy",
+    Hardness: "5 - 6",
+    SpecificGravity: 5.26,
+    CrystalSystem: "Trigonal",
+    Origin: "Originally named about 300-325 BCE by Theophrastus from the Greek, 'αιματίτις λίθος' ('aematitis lithos') for 'blood stone'. Translated in 79 by Pliny the Elder to haematites, 'bloodlike', in allusion to the vivid red colour of the powder.",
+    Locality: ["Adana", "Adıyaman", "Ankara", "Artvin", "Bingöl", "Bursa", "Çanakkale", "Denizli", "Hatay", "Sivas", "Tokat"]
+  }
+};
+
+export const malachiteProperties = {
+  Malachite: {
+    Name: "Malachite", 
+    Formula: "Cu2(CO3)(OH)2",
+    Contents: "Copper, Carbon, Hydrogen",
+    Colour: "Green, Yellow-Green, Dark Green",
+    Lustre: "Silky, Earthy",
+    Hardness: "3½ - 4",
+    SpecificGravity: "3.6 - 4.05",
+    CrystalSystem: "Monoclinic",
+    Origin: "Named in antiquity (see Pliny the Elder, 79 CE) molochitus after the Greek μαλαχή, 'mallows,' in allusion to the green color of the leaves. Known in the new spelling, malachites, at least by 1661.",
+    Locality: ["Adana", "Afyon", "Burdur", "Diyarbakır", "Elazığ", "Gaziantep", "Kayseri", "Rize", "Trabzon"]
+  }
+};
+
+export const pyriteProperties = {
+  Pyrite: {
+    Name: "Pyrite", 
+    Formula: "FeS2",
+    Contents: "Iron, Sulphur",
+    Colour: "Brass-Yellow",
+    Lustre: "Metallic",
+    Hardness: "6 - 6½",
+    SpecificGravity: "4.8 - 5",
+    CrystalSystem: "Isometric",
+    Origin: "Named in antiquity from the Greek 'pyr' for 'fire', because sparks flew from it when struck with another mineral or metal. Known to Dioscorides (~50 CE) under the name 'περι υληζ ιατρικηζ' which included both pyrite and chalcopyrite.",
+    Locality: ["Uşak", "Tunceli", "Yozgat", "Ordu"]
+  }
+};
+
+export const pyromorphiteProperties = {
+  Pyromorphite: {
+    Name: "Pyromorphite", 
+    Formula: "Pb5(PO4)3Cl",
+    Contents: "Lead, Phosphorus, Oxygen, Carbon",
+    Colour: "Green, Dark green, Green-Yellow, Orange-Yellow, Brown, White, Colorless",
+    Lustre: "Sub-Vitreous, Resinous, Waxy, Greasy",
+    Hardness: "3½ - 4",
+    SpecificGravity: 7.04,
+    CrystalSystem: "Hexagonal",
+    Origin: "Originally called Grön Blyspat and Minera plumbi viridis by Johan Gottschalk Wallerius in 1748 and, later, Mine de plumb verte in 1753. Named pyromorphite in 1813 by Johann Friedrich Ludwig Hausmann from the Greek for 'fire' and 'form', because, after being melted into a globule, a sample will begin to take on a crystalline shape during cooling.",
+    Locality: ["İzmir"]
+  }
+};
+
+export const quartzProperties = {
+  Quartz: {
+    Name: "Quartz", 
+    Formula: "SiO2",
+    Contents: "Silicone, Oxygen",
+    Colour: "Colorless, Prism",
+    Lustre: "Vitreous",
+    Hardness: 7,
+    SpecificGravity: "2.65 - 2.66",
+    CrystalSystem: "Trigonal",
+    Origin: "The most ancient name known is recorded by Theophrastus in about 300-325 BCE, κρύσταλλος or kristallos. The varietal names, rock crystal and bergcrystal, preserve the ancient usage. The root words κρύοσ signifying ice cold and στέλλειυ solidify suggest the ancient belief that kristallos was permanently solidified ice. ",
+    Locality: ["Adana", "Ağrı", "Ankara", "Antalya", "Artvin", "Balıkesir", "Bilecik", "Bingöl", "Bitlis", "Burdur", "Çanakkale", "Diyarbakır", "Elazığ", "Erzincan", "Eskişehir", "İstanbul", "Gaziantep", "Kayseri", "Kütahya", "Malatya", "Samsun", "Sivas", "Tuncei", "Uşak", "Zonguldak"]
+  }
+};
+
+export const wulfeniteProperties = {
+  Wulfenite: {
+    Name: "Wulfenite", 
+    Formula: "Pb(MoO4)",
+    Contents: "Lead, Molybdenum, Oxygen",
+    Colour: "Orange-Yellow, Yellow, Honey-Yellow, Red-Orange",
+    Lustre: "Adamantine, Sub-Adamantine, Resinous",
+    Hardness: "2½ - 3",
+    SpecificGravity: "6.5 - 7.5",
+    CrystalSystem: "Tetragonal",
+    MemberOf: "Scheelite Group",
+    Origin: "Renamed in 1845 by Wilhelm Karl von Haidinger in honor of Franz Xaver Freiherr von Wulfen, who authored a monograph on the lead ores of Bleiberg in Austria. This mineral was originally named 'plumbum spatosum flavo-rubrum, ex Annaberg, Austria' in 1772 by Ignaz von Born. In 1781, Joseph Franz Edler von Jacquin called the mineral 'Kärntherischer bleispath'. Other names were later proposed.",
+    Locality: ["Balıkesir", "Çanakkale", "Gümüşhane"]
+  }
+};
+
